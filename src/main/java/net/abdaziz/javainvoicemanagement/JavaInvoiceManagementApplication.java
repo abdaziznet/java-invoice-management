a1e48fb744cf5fd969460a5738a8a201ae3ba409
Abdul Aziz <dev.abdaziz@outlook.com>
2022-08-02 16:26:23 +0700

    starter project

---
+++ src/main/java/net/abdaziz/javainvoicemanagement/JavaInvoiceManagementApplication.java
@@ -0,0 +1,13 @@
+package net.abdaziz.javainvoicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInvoiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInvoiceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/abdaziz/javainvoicemanagement/JavaInvoiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.abdaziz.javainvoicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInvoiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
